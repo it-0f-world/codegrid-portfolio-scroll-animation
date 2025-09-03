a8073aafea444787caafbc63d196e03440af7464
it 0f world <noname.from.milkyway@gmail.com>
2025-09-03 16:18:53 +1000

    initial commit

---
+++ src/script.js
@@ -0,0 +1,122 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Lenis from "lenis";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const config = {
+    gap: 0.08,
+    speed: 0.3,
+    arcRadius: 500,
+};
+
+const spotlightItems = [
+    { name: "city", img: "./img/city.jpg" },
+    { name: "hero", img: "./img/hero.jpg" },
+    { name: "mountain", img: "./img/mountain.jpg" },
+    { name: "ring", img: "./img/ring.jpg" },
+    { name: "street", img: "./img/street.jpg" },
+    { name: "sunset", img: "./img/sunset.jpg" },
+    { name: "waves", img: "./img/waves.jpg" },
+    { name: "hexa", img: "./img/hexa.jpg" },
+    { name: "triangle", img: "./img/triangle.jpg" },
+];
+
+const lenis = new Lenis();
+lenis.on("scroll", ScrollTrigger.update);
+gsap.ticker.add((time) => lenis.raf(time * 1000));
+gsap.ticker.lagSmoothing(0);
+
+const titlesContainer = document.querySelector(".spotlight-titles");
+const imagesContainer = document.querySelector(".spotlight-images");
+const spotlightHeader = document.querySelector(".spotlight-header");
+const titlesContainerElement = document.querySelector(".spotlight-titles-container");
+const introTextElements = document.querySelectorAll(".spotlight-intro-text");
+const imageElements = [];
+
+spotlightItems.forEach((item, index) => {
+    const titleElement = document.createElement("h1");
+    titleElement.textContent = item.name;
+    if (index === 0) titleElement.style.opacity = "1";
+    titlesContainer.appendChild(titleElement);
+
+    const imgWrapper = document.createElement("div");
+    imgWrapper.className = ("spotlight-img");
+    const imgElement = document.createElement("img");
+    imgElement.src = item.img;
+    imgElement.alt = "";
+    imgWrapper.appendChild(imgElement);
+    imagesContainer.appendChild(imgWrapper);
+    imageElements.push(imgWrapper);
+})
+
+const titleElements = titlesContainer.querySelectorAll("h1");
+let currentActiveIndex = 0;
+
+const containerWidth = window.innerWidth * 0.3;
+const containerHeight = window.innerHeight;
+const arcStartX = containerWidth - 220;
+const arcStartY = -200;
+const arcEndY = containerHeight + 200;
+const arcControlPointX = arcStartX - config.arcRadius;
+const arcControlPointY = containerHeight / 2;
+
+function getBezierPozition(t) {
+    const x =
+        (1 - t) * (1 - t) * arcStartX +
+        2 * (1 - t) * t * arcControlPointX +
+        t * t * arcStartX;
+    const y =
+        (1 - t) * (1 - t) * arcStartY +
+        2 * (1 - t) * t * arcControlPointY +
+        t * t * arcEndY;
+    return { x, y };
+}
+
+function getImgProgressState (index, overallProgress) {
+    const startTime = index * config.gap;
+    const endTime = startTime + config.speed;
+    if (overallProgress < startTime) return -1;
+    if (overallProgress > endTime) return 2;
+    return (overallProgress - startTime) / config.speed;
+}
+
+imageElements.forEach((img) => gsap.set(img, { opacity:0 }));
+
+ScrollTrigger.create({
+    trigger: ".spotlight",
+    start: "top top",
+    end: `+=${window.innerHeight * 10}px`,
+    pin: true,
+    pinSpacing: true,
+    scrub: 1,
+    onUpdate: (self) => {
+        const progress = self.progress;
+        if(progress <= 0.2) {
+            const animationProgress = progress / 0.2;
+            const moveDistance = window.innerWidth * 0.6;
+            gsap.set(introTextElements[0], {
+                x: -animationProgress * moveDistance,
+            });
+            gsap.set(introTextElements[1], {
+                x: animationProgress * moveDistance,
+            });
+
+            gsap.set(introTextElements[0], { opacity: 1 });
+            gsap.set(introTextElements[1], { opacity: 1 });
+
+            gsap.set(".spotlight-bg-img", {
+                transform: `scale(${animationProgress})`
+            });
+            gsap.set(".spotlight-bg-img img", {
+                transform: `scale(${1.5 - animationProgress * 0.5})`,
+            });
+            imageElements.forEach((img) => gsap.set(img, { opacity:0 }));
+            spotlightHeader.style.opacity = "0";
+            gsap.set(titlesContainerElement, {
+                "--before-opacity": "0",
+                "--after-opacity": "0",
+            });
+        }
+    }
+})
